45afbf7b9d694cf6e164bf9dea84c0179e5f85f0
Yago Calixto <140445576+Calixtoyago@users.noreply.github.com>
2024-08-13 10:55:45 -0300

    Delete questão12.py

--- questão12.py
+++
@@ -1,9 +0,0 @@
-vogais = ('a', 'e', 'i', 'o', 'u')
-string = input('Digite algo: ').lower().strip()
-
-count = 0
-for c in string:
-    if c in vogais:
-        count += 1
-
-print(f'A string fornecida possui {count} vogais')

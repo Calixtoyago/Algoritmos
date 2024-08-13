29c3a8f55e42af964f64072897862caf7f8d7db8
Yago Calixto <140445576+Calixtoyago@users.noreply.github.com>
2024-08-13 10:56:22 -0300

    Delete questão14.py

--- questão14.py
+++
@@ -1,9 +0,0 @@
-numeros = []
-while True:
-    num = int(input('Digite um número [999 p/ parar]: '))
-    if num == 999:
-        break
-    numeros.append(num)
-
-print(numeros)
-print(f'O maior número dessa lista é {max(numeros)}')

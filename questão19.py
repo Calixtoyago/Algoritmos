9d6300dbdafe500197c12bf8fa95a203986d3ccf
Yago Calixto <140445576+Calixtoyago@users.noreply.github.com>
2024-08-13 10:57:03 -0300

    Delete questão19.py

--- questão19.py
+++
@@ -1,7 +0,0 @@
-palavra = input('Digite uma palavra: ').strip()
-inverso = palavra[::-1].lower()
-
-if palavra.lower() == inverso:
-    print(f'A palavra {palavra} é um palíndromo.')
-else:
-    print(f'A palavra {palavra} não é um palíndromo.')

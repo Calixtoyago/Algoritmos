f05a406a0092ad625fb1c29cfc86b49f62127569
Yago Calixto <140445576+Calixtoyago@users.noreply.github.com>
2024-08-13 10:58:08 -0300

    Delete questão9.py

--- questão9.py
+++
@@ -1,8 +0,0 @@
-from sympy import isprime
-
-num = int(input('Informe um número inteiro: '))
-print(f'O número {num} ', end='')
-if isprime(num):
-    print('é primo')
-else:
-    print('não é primo')

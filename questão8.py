d6933718a9c798b6446f38d6b6a4e9ad79a78eff
Yago Calixto <140445576+Calixtoyago@users.noreply.github.com>
2024-08-13 10:58:02 -0300

    Delete questão8.py

--- questão8.py
+++
@@ -1,9 +0,0 @@
-impares = []
-
-count = 0
-for c in range(0, 101):
-    if c % 2 != 0:
-        impares.append(c)
-        count += 1
-
-print(f'Entre 0 e 100 tem {count} números ímpares, sendo eles: {impares}')
